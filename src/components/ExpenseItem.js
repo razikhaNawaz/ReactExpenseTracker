1830b73d2a1ceeef167eba868efa29a224d27e2b
razikhaNawaz <razikhafaheem02@gmail.com>
2022-12-20 16:52:21 +0530

    second commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
+    const expenseDate=new Date(2022, 1, 20);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=250;
+    const expenseLocation='bangalore';
     return (
-        <div>
-        <div>Expense Items</div>
-           <div>Food-Rs 50</div>
-           <div>Petrol-Rs 100</div>
-           <div>Movies-Rs 500</div>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <h3>{expenseLocation}</h3>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
+        
         </div>
-    )
+    );
 }
 export default ExpenseItem;
