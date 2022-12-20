ff980907138c3e33a42e7a7c53c18b69d2306a32
razikhaNawaz <razikhafaheem02@gmail.com>
2022-12-20 13:39:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+   return (
+    <div>
+      <h2>lets get started</h2>
+      <ExpenseItem></ExpenseItem>
+
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div>
+        <div>Expense Items</div>
+           <div>Food-Rs 50</div>
+           <div>Petrol-Rs 100</div>
+           <div>Movies-Rs 500</div>
+        </div>
+    )
+}
+export default ExpenseItem;
